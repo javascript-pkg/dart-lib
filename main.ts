358b6ac481db267c755924550cad3229108c7b4a
JavaCommons Technologies <javacommons@gmail.com>
2026-01-13 19:54:15 +0900

    dart-lib-v2026.0113.1954.15

---
+++ main.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env -S deno run
+import { readLines } from "https://deno.land/std@0.224.0/io/mod.ts";
+
+export async function getStdin(defaultStr?: string) {
+  if (Deno.isatty(Deno.stdin.rid)) {
+    return defaultStr ?? "";
+  }
+  const stdin: string[] = [];
+  for await (const line of readLines(Deno.stdin)) {
+    stdin.push(line);
+  }
+  return stdin.join("\n");
+}
+
+console.log(await getStdin());
