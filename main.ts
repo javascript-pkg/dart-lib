280eb3b9129fe0291e7d72f76c4bbd17c7819874
JavaCommons Technologies <javacommons@gmail.com>
2026-01-13 20:19:41 +0900

    .

--- main.ts
+++ main.ts
@@ -1,19 +1,4 @@
 #! /usr/bin/env deno-run
-//import { readLines } from "https://deno.land/std@0.224.0/io/mod.ts";
 import { getStdin } from "./stdinReader.ts";
 
-/*
-export async function getStdin(defaultStr?: string) {
-  if (Deno.isatty(Deno.stdin.rid)) {
-    return defaultStr ?? "";
-  }
-  const stdin: string[] = [];
-  for await (const line of readLines(Deno.stdin)) {
-    stdin.push(line);
-  }
-  return stdin.join("\n");
-}
-
-console.log(await getStdin());
-*/
 console.log(await getStdin());
